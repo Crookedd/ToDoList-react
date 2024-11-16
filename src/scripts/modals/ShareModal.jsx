98d76bfd6f2637267603573b6f829630d7cbedbe
LAPTOP-ULCU732A\User <valeri.ignateva@list.ru>
2024-11-16 17:56:33 +0300

    fixed share modal

--- src/scripts/modals/ShareModal.jsx
+++ src/scripts/modals/ShareModal.jsx
@@ -5,7 +5,38 @@ import TelegramImg from "/src/assets/images/telegram.svg";
 import WhatsAppImg from "/src/assets/images/whats.svg";
 
 const ShareModal = ({ task, onClose }) => {
-  const copyTaskToClipboard = () => {
+
+  const shareLinks = [
+    {
+      name: "Копировать",
+      image: CopyImg,
+      onClick: () => copyTaskToClipboard(task),
+    },
+    {
+      name: "VK",
+      image: VKImg,
+      onClick: () => shareOnSocialMedia(task, "https://vk.com/share.php?url="),
+    },
+    {
+      name: "Telegram",
+      image: TelegramImg,
+      onClick: () => shareOnSocialMedia(task, "https://t.me/share/url?url="),
+    },
+    {
+      name: "WhatsApp",
+      image: WhatsAppImg,
+      onClick: () => shareOnSocialMedia(task, "https://wa.me/?text="),
+    },
+  ];
+
+  const shareOnSocialMedia = (task, baseUrl) => {
+    const url = encodeURIComponent(
+      `Задача: ${task.title}\nОписание: ${task.about}`
+    );
+    window.open(`${baseUrl}${url}`, "_blank");
+  };
+
+  const copyTaskToClipboard = (task) => {
     const taskText = `Задача: ${task.title}\nОписание: ${task.about}`;
     navigator.clipboard
       .writeText(taskText)
@@ -17,43 +48,19 @@ const ShareModal = ({ task, onClose }) => {
       });
   };
 
-  const shareOnTelegram = () => {
-    const url = encodeURIComponent(
-      `Задача: ${task.title}\nОписание: ${task.about}`
-    );
-    window.open(`https://t.me/share/url?url=${url}`, "_blank");
-  };
-
-  const shareOnWhatsApp = () => {
-    const url = encodeURIComponent(
-      `Задача: ${task.title}\nОписание: ${task.about}`
-    );
-    window.open(`https://wa.me/?text=${url}`, "_blank");
-  };
-
-  const shareOnVK = () => {
-    const url = encodeURIComponent(
-      `Задача: ${task.title}\nОписание: ${task.about}`
-    );
-    window.open(`https://vk.com/share.php?url=${url}`, "_blank");
-  };
-
   return (
     <div className="modal" onClick={onClose}>
       <div className="edit_modal_content" onClick={(e) => e.stopPropagation()}>
         <div className="share_buttons">
-          <button className="share_button" onClick={copyTaskToClipboard}>
-            <img src={CopyImg} alt="Copy" />
-          </button>
-          <button className="share_button" onClick={shareOnVK}>
-            <img src={VKImg} alt="VK" />
-          </button>
-          <button className="share_button" onClick={shareOnTelegram}>
-            <img src={TelegramImg} alt="Telegram" />
-          </button>
-          <button className="share_button" onClick={shareOnWhatsApp}>
-            <img src={WhatsAppImg} alt="WhatsApp" />
-          </button>
+          {shareLinks.map((link, index) => (
+            <button
+              key={index}
+              className="share_button"
+              onClick={link.onClick}
+            >
+              <img src={link.image} alt={link.name} />
+            </button>
+          ))}
         </div>
       </div>
     </div>
